68202fd3831064a6dad256bd80d6dfe565c313b6
ghadfield32 <ghadfield32@gmail.com>
2023-08-25 07:31:21 -0400

    firxing openai and streamlist cache error

--- llama_app.py
+++ llama_app.py
@@ -17,14 +17,19 @@ if "messages" not in st.session_state.keys():
         {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
     ]
 
-@st.cache_resource(show_spinner=False)
+@st.cache_resource(show_spinner=False, ttl=3600)  # Cache for an hour
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-        return index
+    try:
+        with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+            reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+            docs = reader.load_data()
+            service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+            index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+            return index
+    except openai.error.RateLimitError:
+        st.error("We've hit the OpenAI API rate limit. Please try again later.")
+        return None  # Return None or an empty structure if rate limit is hit
+
 
 index = load_data()
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
