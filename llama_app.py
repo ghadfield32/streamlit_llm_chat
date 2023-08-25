6d5e56612eeb6c795b42107cc5661adf56bf7719
ghadfield32 <ghadfield32@gmail.com>
2023-08-25 07:24:13 -0400

    added running limits

--- llama_app.py
+++ llama_app.py
@@ -3,14 +3,15 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
+import time  # For adding delay
+from openai.error import OpenAIError  # To catch specific OpenAI errors
 
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-#openai.api_key = st.secrets.openai_key
 openai.api_key = st.secrets["openai_key"]
 st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
 st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
-         
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+
+if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
     ]
@@ -25,22 +26,26 @@ def load_data():
         return index
 
 index = load_data()
-# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
+for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
-
+            try:
+                response = chat_engine.chat(prompt)
+                st.write(response.response)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message)
+            except OpenAIError as e:  # Catch OpenAI specific errors
+                if "RateLimitError" in str(e):  # Check if it's a rate limit error
+                    st.write("Sorry, I'm getting too many requests right now. Please wait a moment and try again.")
+                    time.sleep(10)  # You can adjust this delay as needed
+                else:
+                    st.write("Sorry, there was an error processing your request.")
