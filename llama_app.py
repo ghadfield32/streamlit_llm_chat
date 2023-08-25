c44e2541227c84f23c6c230c0903d9605260747a
ghadfield32 <ghadfield32@gmail.com>
2023-08-25 07:44:37 -0400

    errors

--- llama_app.py
+++ llama_app.py
@@ -3,21 +3,34 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
-from openai.error import OpenAIError  # To catch specific OpenAI errors
-import tenacity  # Import the tenacity library for its RetryError
+from openai.error import OpenAIError
+import tenacity
 
-st.set_page_config(page_title="Geoff's attempt at a Llama chat app", ...)
+st.set_page_config(
+    page_title="Chat with the Streamlit docs, powered by LlamaIndex",
+    page_icon="🦙",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None
+)
 
 openai.api_key = st.secrets["openai_key"]
+
 st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.info(
+    "Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)",
+    icon="📃"
+)
 
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {
+            "role": "assistant",
+            "content": "Ask me a question about Streamlit's open-source Python library!"
+        }
     ]
 
-@st.cache_resource(show_spinner=False, ttl=3600)  # Cache for an hour
+@st.cache_resource(show_spinner=False, ttl=3600)
 @tenacity.retry(wait=tenacity.wait_fixed(60), stop=tenacity.stop_after_attempt(3), reraise=True)
 def load_data():
     try:
@@ -25,7 +38,8 @@ def load_data():
             reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
             docs = reader.load_data()
             service_context = ServiceContext.from_defaults(
-                llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, ...))
+                llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="... (rest of your prompt)")
+            )
             index = VectorStoreIndex.from_documents(docs, service_context=service_context)
             return index
     except openai.error.RateLimitError:
@@ -33,30 +47,29 @@ def load_data():
         return None
 
 index = load_data()
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Your question"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+if index:
+    chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+    if prompt := st.chat_input("Your question"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            try:
-                response = chat_engine.chat(prompt)
-                st.write(response.response)
-                message = {"role": "assistant", "content": response.response}
-                st.session_state.messages.append(message)
-            except tenacity.RetryError as e:
-                # Check if the underlying exception is a RateLimitError
-                if "RateLimitError" in str(e.last_attempt.exception()):
-                    st.write("Sorry, I'm getting too many requests right now. Please wait a moment and try again.")
-                else:
-                    st.write("Sorry, there was an error processing your request.")
-            except Exception as e:  # Catch any other general exceptions
-                st.write(f"An unexpected error occurred: {e}")
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                try:
+                    response = chat_engine.chat(prompt)
+                    st.write(response.response)
+                    message = {"role": "assistant", "content": response.response}
+                    st.session_state.messages.append(message)
+                except tenacity.RetryError as e:
+                    if "RateLimitError" in str(e.last_attempt.exception()):
+                        st.write("Sorry, I'm getting too many requests right now. Please wait a moment and try again.")
+                    else:
+                        st.write("Sorry, there was an error processing your request.")
+                except Exception as e:
+                    st.write(f"An unexpected error occurred: {e}")
 
