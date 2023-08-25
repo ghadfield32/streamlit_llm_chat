0b1e6e6eff07058d3129693972038b34d27f1db1
ghadfield32 <ghadfield32@gmail.com>
2023-08-25 13:45:33 -0400

    openai usage errors input

--- llama_app.py
+++ llama_app.py
@@ -15,16 +15,11 @@ openai.api_key = st.secrets["openai_key"]
 
 # Display title and info.
 st.title("💬 Chatbot")
-st.write("Welcome to the futuristic I, Robot themed chatbot! How can I assist you today?")
+st.write("Welcome to the futuristic chatbot! How can I assist you today?")
 
 # Initialize session state for messages if not already done.
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-# Initialize the API call counter if it doesn't exist
-MAX_PROMPTS = 10  # adjust according to your needs
-if "api_calls" not in st.session_state:
-    st.session_state["api_calls"] = 0
+    st.session_state["messages"] = []
 
 # Display previous messages.
 for msg in st.session_state.messages:
@@ -32,14 +27,11 @@ for msg in st.session_state.messages:
 
 # When the user submits a new question or statement.
 if prompt := st.chat_input():
-    if st.session_state["api_calls"] < MAX_PROMPTS:
-        # Make the API call
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    try:
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages + [{"role": "user", "content": prompt}])
         msg = response.choices[0].message
+        st.session_state.messages.append({"role": "user", "content": prompt})
         st.session_state.messages.append(msg)
         st.chat_message("assistant").write(msg.content)
-
-        # Increment the counter
-        st.session_state["api_calls"] += 1
-    else:
+    except openai.error.RateLimitError:
         st.warning("Sorry, we have reached the maximum number of requests for now. Please try again later.")
