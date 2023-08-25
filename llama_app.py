084a60326ee055b03f08562a7cd90832073a8d20
ghadfield32 <ghadfield32@gmail.com>
2023-08-25 08:10:20 -0400

    upgraded to include premium benefits of gpt

--- llama_app.py
+++ llama_app.py
@@ -6,6 +6,7 @@ from llama_index import SimpleDirectoryReader
 from openai.error import OpenAIError
 import tenacity
 
+# Page configuration.
 st.set_page_config(
     page_title="Chat with the Streamlit docs, powered by LlamaIndex",
     page_icon="🦙",
@@ -14,14 +15,17 @@ st.set_page_config(
     menu_items=None
 )
 
+# Set the OpenAI API key.
 openai.api_key = st.secrets["openai_key"]
 
+# Display title and info.
 st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
 st.info(
     "Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)",
     icon="📃"
 )
 
+# Initialize session state for messages if it doesn't exist.
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {
@@ -30,8 +34,9 @@ if "messages" not in st.session_state.keys():
         }
     ]
 
+# Cached data loader with adjusted retry mechanism for GPT Premium's higher rate limits.
 @st.cache_resource(show_spinner=False, ttl=3600)
-@tenacity.retry(wait=tenacity.wait_fixed(60), stop=tenacity.stop_after_attempt(3), reraise=True)
+@tenacity.retry(wait=tenacity.wait_fixed(30), stop=tenacity.stop_after_attempt(5), reraise=True) # Reduced wait time due to higher rate limits for premium users.
 def load_data():
     try:
         with st.spinner(text="Loading and indexing the Streamlit docs..."):
@@ -43,23 +48,28 @@ def load_data():
             index = VectorStoreIndex.from_documents(docs, service_context=service_context)
             return index
     except openai.error.RateLimitError:
-        st.error("We've hit the OpenAI API rate limit. Please try again later.")
+        st.error("We've hit the OpenAI API rate limit. Please try again in a bit.")
         return None
 
 index = load_data()
 
+# Proceed if index is loaded.
 if index:
     chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+    # Get user input.
     if prompt := st.chat_input("Your question"):
         st.session_state.messages.append({"role": "user", "content": prompt})
 
+    # Display the chat history.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
+    # Respond to the user's message if it's the latest.
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
+            with st.spinner("Thinking..."): # Since premium provides faster response times, the user experience will be smoother.
                 try:
                     response = chat_engine.chat(prompt)
                     st.write(response.response)
@@ -67,7 +77,7 @@ if index:
                     st.session_state.messages.append(message)
                 except tenacity.RetryError as e:
                     if "RateLimitError" in str(e.last_attempt.exception()):
-                        st.write("Sorry, I'm getting too many requests right now. Please wait a moment and try again.")
+                        st.write("Sorry, I'm getting too many requests right now. Please wait a bit and try again.")
                     else:
                         st.write("Sorry, there was an error processing your request.")
                 except Exception as e:
